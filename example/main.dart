12861f9f8c2fd81a4a3696c8dc7f0a8c00149d27
Victor Cassiano <43105344+Victorcassiano@users.noreply.github.com>
2020-09-15 05:29:29 -0300

    Update main.dart

--- example/main.dart
+++ example/main.dart
@@ -1,9 +1,11 @@
 import 'package:conversor/src/conversor.dart';
 
 void main() {
-  //
   var num = 158;
   print(ConversorBases.toBinary(num));
   print(ConversorBases.toHexadecimal(num));
   print(ConversorBases.toOcta(num));
+  
+  //Saída: Binário: 10011110, Hexadecimal: 9E, Octa: 236
+
 }
